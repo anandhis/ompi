038fc841cf1a7bc95c56e55c9a1aec68d85590b4
Anandhi S Jayakumar <anandhi.s.jayakumar@intel.com>
2016-03-18 15:57:10 -0700

    commiting OFI plugin changes and query_fabric interface

--- orte/mca/rml/rml_types.h
+++ orte/mca/rml/rml_types.h
@@ -167,8 +167,8 @@ BEGIN_C_DECLS
 
 /*** RML OFI keys ***/
 #define ORTE_OFI_START_KEY 	ORTE_QOS_MAX_KEY
-#define ORTE_PROV_NAME		(ORTE_QOS_MAX_KEY + 1)  	//char * - null terminated string containing provider name
-#define ORTE_PROTOCOL           (ORTE_QOS_MAX_KEY + 2)          //uint32 - protocol type as returned by fi_info
+#define ORTE_PROV_NAME		(ORTE_OFI_START_KEY + 1)  	//char * - null terminated string containing provider name
+#define ORTE_PROTOCOL           (ORTE_OFI_START_KEY + 2)          //uint32 - protocol type as returned by fi_info
 
 #define ORTE_RML_TAG_NTOH(t) ntohl(t)
 #define ORTE_RML_TAG_HTON(t) htonl(t)
